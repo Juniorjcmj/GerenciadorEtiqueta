6bd2e74e4f08addc7a480a56be80cc9e7ab794d4
Jotanaval <jotacfs2010@hotmail.com>
2020-08-16 21:45:51 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -36,13 +45,32 @@ public class MainViewController implements Initializable {
 	
 	@FXML 
 	public void onMenuItemManualAction() {
-		System.out.println("onMenuItemManualAction");
+		loadView("/gui/Manual.fxml");
 	}
 	
 	
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {		
+	}
+	
+	private synchronized void loadView(String absoluteName) {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newBox = loader.load();
+			
+			Scene maiScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)maiScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newBox.getChildren());
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error Loading view", e.getMessage(),AlertType.ERROR);
+		}
+		
 	}
 
 	
